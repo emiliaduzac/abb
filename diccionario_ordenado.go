bd73161a0bb7f046a374c37a64af20eb32d214d2
emiliaduzac <114706083+emiliaduzac@users.noreply.github.com>
2023-05-18 21:08:40 -0300

    Update diccionario_ordenado.go

--- diccionario_ordenado.go
+++ diccionario_ordenado.go
@@ -9,11 +9,11 @@ type DiccionarioOrdenado[K comparable, V any] interface {
 
 	// Obtener devuelve el dato asociado a una clave. Si la clave no pertenece, debe entrar en pánico con mensaje
 	// 'La clave no pertenece al diccionario'
-	//Obtener(clave K) V
+	Obtener(clave K) V
 
 	// Borrar borra del Diccionario la clave indicada, devolviendo el dato que se encontraba asociado. Si la clave no
 	// pertenece al diccionario, debe entrar en pánico con un mensaje 'La clave no pertenece al diccionario'
-	//Borrar(clave K) V
+	Borrar(clave K) V
 
 	// Cantidad devuelve la cantidad de elementos dentro del diccionario
 	Cantidad() int
@@ -27,8 +27,8 @@ type DiccionarioOrdenado[K comparable, V any] interface {
 
 	// IterarRango itera sólo incluyendo a los elementos que se encuentren comprendidos en el rango indicado,
 	// incluyéndolos en caso de encontrarse
-	//IterarRango(desde *K, hasta *K, visitar func(clave K, dato V) bool)
+	IterarRango(desde *K, hasta *K, visitar func(clave K, dato V) bool)
 
 	// IteradorRango crea un IterDiccionario que sólo itere por las claves que se encuentren en el rango indicado
-	//IteradorRango(desde *K, hasta *K) IterDiccionario[K, V]
+	IteradorRango(desde *K, hasta *K) IterDiccionario[K, V]
 }
